6eb1669a6b7ca8212697ab954c3560c0ae8b19b6
Abdo Sobhy <114699244+abdoSobhy1@users.noreply.github.com>
2025-05-14 17:51:13 +0300

    enhancements

--- app/(home)/components/contact-us.tsx
+++ app/(home)/components/contact-us.tsx
@@ -3,6 +3,7 @@
 import MainButton from "@/components/main-button";
 import Slot from "@/components/slot";
 import ContactForm from "../../../components/contact-form";
+import Image from "next/image";
 
 const slots = [
     { title: "About Us", href: "/about-us" },
@@ -19,8 +20,9 @@ export default function ContactUsForm() {
                     <Slot key={slot.title} title={slot.title} href={slot.href} />
                 ))}</div>
                 <div className="bg-[#1e2334] rounded-[20px] px-8  pt-20 pb-5 text-center">
-                    <p className="text-white text-2xl font-semibold mb-3">Request a Call Back</p>
-                    <p className="text-[#d0cfec] leading-6">You can contact us for a free quote.</p>
+                    <Image src="/images/icons/phoneCall.svg" alt="Phone Call" width={48} height={48} className="mx-auto mb-8" />
+                    <p className="text-white text-2xl font-semibold mb-3">Contact Us</p>
+                    <p className="text-[#d0cfec] leading-6 mb-4">You can contact us for a free quote.</p>
                     <p className="text-white text-2xl font-semibold mb-20">+2 (120) 469 8888</p>
 
                     <MainButton href="/contact-us" className="lg:w-full bg-maroon hover:bg-maroon-700 text-center">Contact Us</MainButton>
