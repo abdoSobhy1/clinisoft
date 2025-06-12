92ab6a413b8d2c4a322c13b706b126bd59af7d8d
Abdo Sobhy <114699244+abdoSobhy1@users.noreply.github.com>
2025-06-12 18:11:06 +0300

    new sizing system

--- components/specialty-card.tsx
+++ components/specialty-card.tsx
@@ -11,12 +11,14 @@ export default function SpecialtyCard({ title, description, icon }: SpecialtyCar
     return (
         <div className="rounded-3xl bg-white p-4 shadow-md h-full flex flex-col justify-between">
             <div className="flex items-center gap-6">
-                <div className="rounded-lg bg-[#EBFDFF] border-2 border-[#BAE5E9] aspect-square w-[90px] flex items-center justify-center">
-                    <Image src={icon} alt={title} width={60} height={60} />
+                <div className="rounded-lg bg-[#EBFDFF] border-2 border-[#BAE5E9] aspect-square w-22 flex items-center justify-center">
+                    <div className="relative w-14 h-14">
+                        <Image src={icon} alt={title} fill className="object-contain" />
+                    </div>
                 </div>
                 <h3 className="text-[#29858D] font-bold fs-var-3xl">{title}</h3>
             </div>
-            <p className="text-[#242424] py-8 leading-8">{description}</p>
+            <p className="text-[#242424] py-8 leading-8 fs-var-base">{description}</p>
             <MainButton href={`/specialties/${title.toLowerCase().replace(/\s+/g, '-')}`} className="bg-teal hover:bg-teal-700 text-center">Explore More</MainButton>
         </div>
     );
