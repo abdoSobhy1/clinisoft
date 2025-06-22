52e478c9446050f52510c22b41fbef1f1778721f
Abdo Sobhy <114699244+abdoSobhy1@users.noreply.github.com>
2025-06-22 16:53:51 +0300

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -31,11 +31,13 @@ export function generateStaticParams() {
 
 export default async function LocaleLayout({
   children,
-  params: { locale }
+  params
 }: Readonly<{
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }>) {
+
+  const { locale } = await params;
 
   let messages
   try {
