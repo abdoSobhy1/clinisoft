a315aa6264c4e0d64c45e6bedc349b31fce9c178
Abdo Sobhy <114699244+abdoSobhy1@users.noreply.github.com>
2025-06-22 10:37:59 +0300

    Update hero-slide.tsx

--- components/hero-slide.tsx
+++ components/hero-slide.tsx
@@ -1,6 +1,7 @@
 import { CarouselItem } from "./ui/carousel";
 import DemoButton from "./demo-button";
 import { useTranslations } from "next-intl";
+import Image from "next/image";
 
 export default function HeroSlide({
     title,
@@ -15,9 +16,14 @@ export default function HeroSlide({
     return (
         <CarouselItem className="p-0">
             <div
-                className="w-full pl-2 md:pl-0 h-svh flex flex-col items-center justify-center bg-cover bg-center bg-no-repeat relative"
-                style={{ backgroundImage: `url(${image})` }}
+                className="w-full pl-2 md:pl-0 h-svh flex flex-col items-center justify-center relative overflow-hidden"
             >
+                <Image
+                    src={image}
+                    alt={`Hero background for ${title}`}
+                    fill
+                    className="select-none w-full h-full object-cover object-center"
+                />
                 <div className="before:content-[''] before:absolute before:inset-0 before:bg-linear-0 before:from-[#F0FFFF] before:from-10% before:to-[#F0FFFF00] before:to-50% before:z-2 before:pointer-events-none after:content-[''] after:absolute after:inset-0 after:bg-linear-135 after:from-[#29858de6] after:to-[#29858d4d] after:z-1 after:pointer-events-none" />
                 <div className="relative z-5 text-center px-4 -translate-y-1/2 md:translate-y-0">
                     <h1 className="leading-10 md:leading-[96px] fs-var-2xl md:fs-var-5xl lg:fs-var-7xl font-bold relative text-[#A2E2E9] bg-linear-0 from-[#A2E2E9] to-[#FFFFFF] supports-[background-clip:text]:text-transparent supports-[background-clip:text]:bg-clip-text">
