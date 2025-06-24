feaf26aee90708a0b5ff0b54a7cbca11d86cdb25
Abdo Sobhy <114699244+abdoSobhy1@users.noreply.github.com>
2025-06-24 11:38:30 +0300

    increased font size for hero section in mobile

--- components/hero-slide.tsx
+++ components/hero-slide.tsx
@@ -26,7 +26,7 @@ export default function HeroSlide({
                 />
                 <div className="before:content-[''] before:absolute before:inset-0 before:bg-linear-0 before:from-[#F0FFFF] before:from-10% before:md:from-20% before:to-[#F0FFFF00] before:to-50% md:before:to-80% before:z-2 before:pointer-events-none after:content-[''] after:absolute after:inset-0 after:bg-linear-135 after:from-[#29858de6] after:to-[#29858d4d] after:z-1 after:pointer-events-none" />
                 <div className="relative z-5 text-center px-4 top-40 md:top-0 mt-10 md:mt-0">
-                    <h1 className="leading-10 md:leading-[96px] fs-var-4xl md:fs-var-5xl lg:fs-var-7xl font-bold relative text-[#A2E2E9] bg-linear-0 from-[#A2E2E9] to-[#FFFFFF] supports-[background-clip:text]:text-transparent supports-[background-clip:text]:bg-clip-text">
+                    <h1 className="leading-10 md:leading-[96px] fs-var-5xl lg:fs-var-7xl font-bold relative text-[#A2E2E9] bg-linear-0 from-[#A2E2E9] to-[#FFFFFF] supports-[background-clip:text]:text-transparent supports-[background-clip:text]:bg-clip-text">
                         {t(title)}
                     </h1>
                     {/* <h1 className="absolute px-4 leading-10 md:leading-[96px] inset-0 fs-var-2xl md:fs-var-5xl lg:fs-var-7xl font-bold -z-1 [text-shadow:_0px_4px_4px_rgb(0_0_0_/_0.25)] select-none">
